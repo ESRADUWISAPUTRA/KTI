9437bdf005baf94b835e8b9f3dea7cbdfead37dc
ESRADUWISAPUTRA <72220576@students.ukdw.ac.id>
2024-10-03 00:19:04 +0700

    first commit

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleSecuredWeb.Models
+{
+    public class Student
+    {
+        public string Nim { get; set;} = null!;
+        public string Name { get; set;} = null!;
+
+    }
+}
